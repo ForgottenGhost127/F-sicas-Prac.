abf00dfaf6deac5740ead15363e72c7e2a0959bc
LAPTOP-0CENV2U2\jufla <judith.legaz@hotmail.com>
2024-10-04 19:44:49 +0200

    Changes...

--- Assets/Scripts/ScriptsLab/PlayerContr.cs
+++ Assets/Scripts/ScriptsLab/PlayerContr.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerContr : MonoBehaviour
 {
     public float speed = 8f;
+    public float rotationSp = 30f;
+    public float rayDistance = 5f;
+
     private Rigidbody rb;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,10 +22,35 @@ public class PlayerContr : MonoBehaviour
 
         Vector3 movimiento = new Vector3(moveHoriz, 0f, moveVert).normalized;
         rb.velocity = movimiento * speed;
+        if (movimiento != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movimiento);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSp);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            shootRaycast();
+        }
     }
 
+    void shootRaycast()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.black, 1f);
+        if(Physics.Raycast(transform.position, transform.forward, out hit, rayDistance))
+        {
+            if(hit.collider.CompareTag("Obstacle"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+            
+        }
+
+    }
+
+
     //Terminar el laberinto: Walls 1,2 y 3 son las indestructibles. Wall 4 serán las walls que podremos destruir con el uso de raycast.
-    //Implementar el Raycast: agregar lógica para disparar un raycast y destruir muros débiles.
+
     //Meta y Trigger: Crear un objeto al final del laberinto con un collider con isTrigger activado para que, cuando el player lo toque, se muestre el mensaje de victoria.
     //Contador de tiempo y Efectos visuales: implementar un contador de tiempo y efectos cuando el jugador destruye obstáculos.
 }
